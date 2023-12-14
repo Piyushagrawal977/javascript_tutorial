4509c920f5ce5711e09844d0b6c4a2f69c83e601
Piyush agrawal <74922899+Piyushagrawal977@users.noreply.github.com>
2023-12-14 10:36:46 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const id=13232;
+let name="Mukesh";
+var email="hc@google.com";
+age=18;
+let professor;
+// id =84883 can't change the const valaue. once it is intialized
+console.log(id);
+
+// prefer not use var because of issue in block scope and fuction scope
+
+console.table([name,email,age,professor])
