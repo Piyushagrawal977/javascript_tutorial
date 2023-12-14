3f365e8540b09145606e5d5ce71186e7a5970c72
Piyush agrawal <74922899+Piyushagrawal977@users.noreply.github.com>
2023-12-14 10:50:15 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+
+/*
+Datatypes
+number = 2 to power 53
+big int
+string
+underfined
+boolean=true/false
+null =>stand alone
+symbol=> used in react, make unique component
+
+object
+*/
+
+
+console.log(typeof "hello")
+
+
+console.log(typeof null);
