4e0b554c560cdce001e07d43db8596e800bb29b5
Piyush agrawal <74922899+Piyushagrawal977@users.noreply.github.com>
2023-12-16 08:55:13 +0000

    date and time

---
+++ 01_basics/07_dateandtime.js
@@ -0,0 +1,8 @@
+let date=new Date();
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toString());
+console.log(date.toLocaleString());
+console.log(new Date("2023-01-14"));
+
+console.log(Math.floor(Date.now()/360000));
