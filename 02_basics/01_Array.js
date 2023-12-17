09c907800cb15ac77b8f254636021cc1e2b26c07
Piyush agrawal <74922899+Piyushagrawal977@users.noreply.github.com>
2023-12-17 01:44:45 +0000

    update array

--- 02_basics/01_Array.js
+++ 02_basics/01_Array.js
@@ -2,6 +2,7 @@ const myArr = [1,2,3,4];
 // console.log(myArr);
 const newArr = new Array("shaktiman","naagraj");
 // console.log(newArr);
+// console.log(typeof newArr);
 
 // to push an element in the array we use push()
 myArr.push(6);
@@ -27,13 +28,41 @@ const arr=myArr.join()      //join() convert the array into string including ','
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)  // slice take a sub array of given parameters but not including the last index of given parameter from the main array
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
 const myn2 = myArr.splice(1, 3)  // it splits the main array, the sub array part gets removed from the main array including the last index of given parameter.
-console.log("C ", myArr);
-console.log(myn2);
+// console.log("C ", myArr);
+// console.log(myn2);
+
+const marvel_heros=["IronMan","Hulk","Thor","Captain America"];
+const dc_heros=["SuperMan","BatMan","Arrow","Flash"];
+
+// marvel_heros.push(dc_heros);    //dc added on the pre-exist array as a part of element
+// console.log(marvel_heros);
+// console.log(marvel_heros[4][2]);  // to get a value of dc element we need to use 2d array indexing [][];
+
+const all_heroes=marvel_heros.concat(dc_heros); // concat() merges the two array, but make a new array not add the elements into the pre-existing array.
+// console.log(all_heroes);
+
+//spread
+const another_all_heros = [...marvel_heros,...dc_heros]  //it uses when need to merge more than two arrays
+// console.log(another_all_heros);
+
+const another_array=[1,2,3,4,[9,8],7,[6,4,[0,3,5]]];
+// console.log(another_array);
+// console.log(another_array.flat(Infinity));   // use to reduce multiple dimension array into single dimension array
+
+console.log(Array.isArray("Helloworld"));
+console.log(Array.from("Helloworld"));
+console.log(Array.from({name:"piyush"})); //converting object into array but needed to provide which element want to convert value or key.
+
+let score1=100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score1,score2,score3));
